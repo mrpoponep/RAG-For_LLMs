33ccf3bc589b1feafd37db7870f541d38f5f8088
Mrpoponep <hotu040304@gmail.com>
2024-06-14 07:41:08 +0700

    Refactoring and adding UI

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from pymilvus import connections, Collection, utility
+from pymilvus import utility
+import google.generativeai as genai
+from local_settings import API_KEY
+from pymilvus import MilvusClient
+from helper_func import create_prompt, process_data
+
+connections.connect(host='localhost', port='19530')
+milvus_client = MilvusClient("http://localhost:19530")
+collection_name = "my_rag_collection"
+
+# Streamlit app
+st.title("Insurance Q&A")
+
+st.write("Ask a question and get an answer:")
+
+embedding_model = 'models/text-embedding-004'
+genai.configure(api_key=API_KEY)
+def embed_fn(title, text):
+  return genai.embed_content(model=embedding_model,
+                             content=text,
+                             task_type="retrieval_document",
+                             title=title)["embedding"]
+
+user_question = st.text_input("Your Question:")
+
+
+if st.button("Get Answer"):
+    search_res = milvus_client.search(
+    collection_name=collection_name,
+    data=[
+        embed_fn(user_question,user_question)
+    ],  # Use the `emb_text` function to convert the question to an embedding vector
+    limit=7,  # Return top 3 results
+    search_params={"metric_type": "IP", "params": {}},  # Inner product distance
+    output_fields=["text"],  # Return the text field
+)
+
+    retrieved_lines_with_distances = [
+        (res["entity"]["text"], res["distance"]) for res in search_res[0]]
+    querry_data= process_data(retrieved_lines_with_distances)
+
+    LLM_model = genai.GenerativeModel('gemini-pro')
+    prompt=create_prompt(querry_data,user_question)
+    print(prompt)
+    response = LLM_model.generate_content(prompt)
+    response =response.text.replace("JSON", "").replace("```","")
+    st.success(f"Answer: {response}")
---
+++ helper_func.py
@@ -0,0 +1,33 @@
+def process_data(data):
+    output=""
+    for item in data:
+        question = item[0][0].strip()
+        answer = item[0][1].strip()
+        output+=f"{question}\n"
+        output+=f"{answer}\n\n"
+    return output
+
+def create_prompt(document, question):
+    PROMPT = f"""
+Task: Answer insurance-related questions accurately and provide some short explanation
+Instructions:
+Read the document carefully.
+Consider key points, concepts, and details within the document.
+Generate questions that seek information or clarification about the document's content.
+Ensure that the questions are grammatically correct and understandable.
+Give the answer directly, do not repeat the question or add any unrelated text
+If the question is not related to the context or there is no answer in the context, return None and do not answer any thing else
+Context:
+{document}
+
+Question:
+{question}
+
+"""
+    return PROMPT
+
+if __name__ == '__main__':
+    data='''Question: How Soon Do You Need To Get Different Auto Insurance After You Move To Another State?
+Answer: The most common time to get new insurance is 30 days . There may be some flexibility however before you can register your auto in the new state you will need to establish your new insurance policy . Also going to long could get you fine should you happen to encounter an officer who is not in a generous mood .'''
+
+    print(create_prompt(data))
